de36b32ccfb3fb2d90dadbc131aaed9775aceda5
Sten T. <102481635+Stensel8@users.noreply.github.com>
2026-09-24 01:16:33 +0200

    feat: test to change default title to 'Kwoooterrrrrr'

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -69,7 +69,7 @@ def page(content,user_id,title,error=None):
     return f"""<!DOCTYPE html>
 <html lang="en-US">
 <head>
-  <title>{title or "Quoter XP"}</title>
+  <title>{title or "Kwoooterrrrrr"}</title>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="/static/style.css">
 </head>
